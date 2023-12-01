e5229dbd53a2ea551e9fb06a36d10b6678f743c7
wise_JG <59231743+Hju95@users.noreply.github.com>
2023-12-01 23:50:03 +0900

    스프링 부트와 JPA 활용1 실습 - 상품 주문 컨트롤러까지

---
+++ src/main/java/jpabook/jpashop/Controller/OrderController.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.Controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberservice;
+    private final ItemService itemservice;
+
+    @GetMapping(value = "/order")
+    public String createForm(Model model) {
+
+        List<Member> members = memberservice.findMembers();
+        List<Item> items = itemservice.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping(value = "/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId, @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
